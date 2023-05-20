814ef9975c4222ce461f64fddd89bc406c5717d8
John Nzyuko <45923438+nzyuko@users.noreply.github.com>
2023-05-20 09:56:15 +0300

    Update make.bat

--- make.bat
+++ make.bat
@@ -10,7 +10,6 @@ SET VERSION=0.0.2
 SET CMD_PKG=github.com/moloch--/denim/cmd
 SET LDFLAGS=-s -w
 SET LDFLAGS=%LDFLAGS% -X %CMD_PKG%.Version=%VERSION%
-SET LDFLAGS=%LDFLAGS% -X %CMD_PKG%.SevenZipURL=https://www.7-zip.org/a/7za920.zip
 SET LDFLAGS=%LDFLAGS% -X %CMD_PKG%.ObfuscatorLLVMURL=https://github.com/moloch--/obfuscator/releases/download/v9.0.1/build.tar.gz
 SET LDFLAGS=%LDFLAGS% -X %CMD_PKG%.NimURL=https://nim-lang.org/download/nim-1.4.2_x64.zip
 
